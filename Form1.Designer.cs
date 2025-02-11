c53b24f3bfa3572383610500dfbce8a7e93c87fe
Lenin Lucas <63570798+leninjo@users.noreply.github.com>
2025-02-10 22:20:31 -0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace LectorHuella
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            AppTitle = new Label();
+            ReaderConnect = new Button();
+            ReadFingeprint = new Button();
+            ShowFingerprint = new PictureBox();
+            AppCloseButton = new Button();
+            AppMinimizeButton = new Button();
+            StatusLabel = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            CopyUUID = new Button();
+            UUIDLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)ShowFingerprint).BeginInit();
+            SuspendLayout();
+            // 
+            // AppTitle
+            // 
+            AppTitle.AutoSize = true;
+            AppTitle.Font = new Font("DM Sans 14pt", 14.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            AppTitle.ForeColor = Color.FromArgb(0, 43, 43, 43);
+            AppTitle.Location = new Point(9, 9);
+            AppTitle.Margin = new Padding(0);
+            AppTitle.Name = "AppTitle";
+            AppTitle.Size = new Size(211, 25);
+            AppTitle.TabIndex = 0;
+            AppTitle.Text = "CDM Lector de Huella";
+            // 
+            // ReaderConnect
+            // 
+            ReaderConnect.FlatAppearance.BorderSize = 0;
+            ReaderConnect.FlatStyle = FlatStyle.Flat;
+            ReaderConnect.Font = new Font("DM Sans 14pt", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ReaderConnect.Location = new Point(207, 77);
+            ReaderConnect.Name = "ReaderConnect";
+            ReaderConnect.Size = new Size(205, 102);
+            ReaderConnect.TabIndex = 9;
+            ReaderConnect.Text = "Verificar lector";
+            ReaderConnect.UseVisualStyleBackColor = false;
+            ReaderConnect.Click += Connect_Click;
+            ReaderConnect.EnabledChanged += (s, e) =>
+            {
+                ReaderConnect.Invalidate();
+            };
+            ReaderConnect.Paint += (s, e) =>
+            {
+                Button btn = (Button)s;
+                e.Graphics.Clear(btn.BackColor);
+
+                if (btn.Enabled)
+                {
+                    btn.BackColor = Color.FromArgb(43, 43, 43);
+                    ReaderConnect.Cursor = Cursors.Hand;
+                }
+                else
+                {
+                    btn.BackColor = Color.FromArgb(70, 70, 70);
+                    ReaderConnect.Cursor = Cursors.No;
+                }
+
+                TextRenderer.DrawText(e.Graphics, btn.Text, btn.Font, btn.ClientRectangle,
+                    btn.Enabled ? Color.White : Color.FromArgb(235, 237, 239), TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+            };
+            // 
+            // ReadFingeprint
+            // 
+            ReadFingeprint.FlatAppearance.BorderSize = 0;
+            ReadFingeprint.FlatStyle = FlatStyle.Flat;
+            ReadFingeprint.Font = new Font("DM Sans 14pt", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ReadFingeprint.ForeColor = Color.White;
+            ReadFingeprint.Location = new Point(207, 185);
+            ReadFingeprint.Name = "ReadFingeprint";
+            ReadFingeprint.Size = new Size(205, 102);
+            ReadFingeprint.TabIndex = 3;
+            ReadFingeprint.Text = "Leer huella";
+            ReadFingeprint.UseVisualStyleBackColor = false;
+            ReadFingeprint.Click += ReadFingeprint_Click;
+            ReadFingeprint.EnabledChanged += (s, e) =>
+            {
+                ReadFingeprint.Invalidate();
+            };
+            ReadFingeprint.Paint += (s, e) =>
+            {
+                Button btn = (Button)s;
+                e.Graphics.Clear(btn.BackColor);
+
+                if (btn.Enabled)
+                {
+                    btn.BackColor = Color.FromArgb(43, 43, 43);
+                    ReadFingeprint.Cursor = Cursors.Hand;
+                }
+                else
+                {
+                    btn.BackColor = Color.FromArgb(70, 70, 70);
+                    ReadFingeprint.Cursor = Cursors.No;
+                }
+
+                TextRenderer.DrawText(e.Graphics, btn.Text, btn.Font, btn.ClientRectangle,
+                    btn.Enabled ? Color.White : Color.FromArgb(235, 237, 239), TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+            };
+            // 
+            // ShowFingerprint
+            // 
+            ShowFingerprint.BorderStyle = BorderStyle.FixedSingle;
+            ShowFingerprint.Location = new Point(12, 77);
+            ShowFingerprint.Margin = new Padding(0);
+            ShowFingerprint.Name = "ShowFingerprint";
+            ShowFingerprint.Size = new Size(189, 210);
+            ShowFingerprint.TabIndex = 5;
+            ShowFingerprint.TabStop = false;
+            // 
+            // AppCloseButton
+            // 
+            AppCloseButton.Cursor = Cursors.Hand;
+            AppCloseButton.FlatAppearance.BorderSize = 0;
+            AppCloseButton.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            AppCloseButton.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            AppCloseButton.FlatStyle = FlatStyle.Flat;
+            AppCloseButton.Font = new Font("DM Sans 14pt", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            AppCloseButton.Image = (Image)resources.GetObject("AppCloseButton.Image");
+            AppCloseButton.Location = new Point(388, 10);
+            AppCloseButton.Margin = new Padding(0);
+            AppCloseButton.Name = "AppCloseButton";
+            AppCloseButton.Size = new Size(25, 25);
+            AppCloseButton.TabIndex = 6;
+            AppCloseButton.UseVisualStyleBackColor = true;
+            AppCloseButton.Click += CloseButton_Click;
+            // 
+            // AppMinimizeButton
+            // 
+            AppMinimizeButton.Cursor = Cursors.Hand;
+            AppMinimizeButton.FlatAppearance.BorderSize = 0;
+            AppMinimizeButton.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            AppMinimizeButton.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            AppMinimizeButton.FlatStyle = FlatStyle.Flat;
+            AppMinimizeButton.Image = (Image)resources.GetObject("AppMinimizeButton.Image");
+            AppMinimizeButton.Location = new Point(362, 10);
+            AppMinimizeButton.Margin = new Padding(0);
+            AppMinimizeButton.Name = "AppMinimizeButton";
+            AppMinimizeButton.Size = new Size(25, 25);
+            AppMinimizeButton.TabIndex = 7;
+            AppMinimizeButton.UseVisualStyleBackColor = true;
+            AppMinimizeButton.Click += MinimizeButton_Click;
+            // 
+            // StatusLabel
+            // 
+            StatusLabel.AutoSize = true;
+            StatusLabel.Font = new Font("DM Sans 14pt", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            StatusLabel.ForeColor = Color.FromArgb(0, 43, 43, 43);
+            StatusLabel.Location = new Point(7, 54);
+            StatusLabel.Name = "StatusLabel";
+            StatusLabel.Size = new Size(0, 20);
+            StatusLabel.TabIndex = 8;
+            // 
+            // button1
+            // 
+            CopyUUID.FlatStyle = FlatStyle.Flat;
+            CopyUUID.Font = new Font("DM Sans 14pt", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            CopyUUID.Location = new Point(300, 355);
+            CopyUUID.Name = "CopyUUID";
+            CopyUUID.Size = new Size(112, 34);
+            CopyUUID.TabIndex = 10;
+            CopyUUID.Text = "Copiar ID";
+            CopyUUID.UseVisualStyleBackColor = true;
+            CopyUUID.Click += CopyUUID_Click;
+            // 
+            // UUIDLabel
+            // 
+            UUIDLabel.AutoSize = true;
+            UUIDLabel.Font = new Font("DM Sans 14pt", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            UUIDLabel.ForeColor = Color.FromArgb(0, 52, 152, 219);
+            UUIDLabel.Location = new Point(34, 311);
+            UUIDLabel.Name = "UUIDLabel";
+            UUIDLabel.Size = new Size(353, 21);
+            UUIDLabel.TabIndex = 11;
+            UUIDLabel.Text = "2b1de602-eaef-4a05-b832-484dd5acbe45";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(424, 401);
+            Controls.Add(UUIDLabel);
+            Controls.Add(CopyUUID);
+            Controls.Add(StatusLabel);
+            Controls.Add(AppMinimizeButton);
+            Controls.Add(AppCloseButton);
+            Controls.Add(ShowFingerprint);
+            Controls.Add(ReadFingeprint);
+            Controls.Add(ReaderConnect);
+            Controls.Add(AppTitle);
+            Font = new Font("DM Sans 14pt", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CDM";
+            Load += MainForm_Load;
+            ((System.ComponentModel.ISupportInitialize)ShowFingerprint).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label AppTitle;
+        private Button ReaderConnect;
+        private Button ReadFingeprint;
+        private PictureBox ShowFingerprint;
+        private Button AppCloseButton;
+        private Button AppMinimizeButton;
+        private Label StatusLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button CopyUUID;
+        private Label UUIDLabel;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+﻿using libzkfpcsharp;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace LectorHuella
+{
+    public partial class Form1 : Form
+    {
+        private bool isReaderInitialized = false;
+        private bool isCheckingReaderStatus = false;
+
+        private readonly zkfp fpInstance = new();
+
+        private readonly Color errorColor = Color.FromArgb(0, 169, 50, 38);
+        private readonly Color successColor = Color.FromArgb(0, 17, 122, 101);
+        private readonly Color loadingColor = Color.FromArgb(0, 36, 113, 163);
+
+        private String UUID = "";
+
+        private readonly Dictionary<int, string> dictionary = new()
+        {
+            {  1,  "La SDK ya está inicializado" },
+            { -1,  "Error al inicializar SDK" },
+            { -2,  "Error al inicializar el lector" },
+            { -3,  "No se encontró ningún dispositivo conectado" },
+            { -4,  "Operación no soportada" },
+            { -5,  "Parámetro inválido en la función" },
+            { -6,  "Error al abrir el dispositivo" },
+            { -7,  "Manejador del dispositivo inválido" },
+            { -8,  "Error al capturar la huella" },
+            { -9,  "Error al extraer la huella dactilar" },
+            { -10, "Proceso abortado" },
+            { -11, "Memoria insuficiente" },
+            { -12, "El dispositivo está ocupado" },
+            { -13, "Error al agregar la huella a la base de datos" },
+            { -14, "Error al eliminar la huella de la base de datos" },
+            { -17, "Fallo en la operación" },
+            { -18, "Operación cancelada" },
+            { -20, "Error al verificar la huella dactilar" },
+            { -22, "Error al combinar varias huellas" },
+            { -23, "El dispositivo no está abierto" },
+            { -24, "La biblioteca no está inicializada" },
+            { -25, "El dispositivo ya está abierto" },
+            { -26, "Error al cargar la imagen de la huella" },
+            { -27, "Error al analizar la imagen de la huella" },
+            { -28, "Tiempo de espera agotado" }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            CheckReaderStatus();
+        }
+
+        private async void CheckReaderStatus()
+        {
+            if (isCheckingReaderStatus) return;
+
+            isCheckingReaderStatus = true;
+            ReaderConnect.Enabled = false;
+
+            StatusLabel.ForeColor = loadingColor;
+            StatusLabel.Text = "Verificando lector...";
+            StatusLabel.Refresh();
+            await Task.Delay(100);
+
+            await Task.Run(() =>
+            {
+                if (!isReaderInitialized)
+                {
+                    int result = fpInstance.Initialize();
+
+                    if (result == zkfperrdef.ZKFP_ERR_OK)
+                    {
+                        isReaderInitialized = true;
+                    }
+                    else
+                    {
+                        this.Invoke(new Action(() =>
+                        {
+                            StatusLabel.ForeColor = errorColor;
+                            StatusLabel.Text = "No se pudo iniciar el lector, verifique conexión.";
+                        }));
+                    }
+                }
+
+                fpInstance.CloseDevice();
+                int deviceCount = fpInstance.GetDeviceCount();
+
+                this.Invoke(new Action(() =>
+                {
+                    if (deviceCount > 0)
+                    {
+                        int result = fpInstance.OpenDevice(0);
+                        if (result == zkfperrdef.ZKFP_ERR_OK)
+                        {
+                            int width = fpInstance.imageWidth;
+                            int height = fpInstance.imageHeight;
+
+                            StatusLabel.ForeColor = successColor;
+                            StatusLabel.Text = $"Lector conectado. Resolución: {width}x{height}";
+                        }
+                        else
+                        {
+                            StatusLabel.ForeColor = errorColor;
+                            StatusLabel.Text = "No fue posible acceder al lector";
+                        }
+                    }
+                    else
+                    {
+                        StatusLabel.ForeColor = errorColor;
+                        StatusLabel.Text = "No se encontró ningún lector conectado.";
+                        fpInstance.Finalize();
+                        isReaderInitialized = false;
+                    }
+                }));
+            });
+
+            isCheckingReaderStatus = false;
+            ReaderConnect.Enabled = true;
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            CheckReaderStatus();
+        }
+
+        private async void ReadFingeprint_Click(object sender, EventArgs e)
+        {
+            ReadFingeprint.Enabled = false;
+
+            StatusLabel.ForeColor = loadingColor;
+            StatusLabel.Text = "Leyendo huella...";
+
+            int size = 2048;
+            byte[] imgBuffer = new byte[fpInstance.imageWidth * fpInstance.imageHeight];
+            byte[] templateBuffer = new byte[size];
+
+            int result = fpInstance.AcquireFingerprint(imgBuffer, templateBuffer, ref size);
+
+            if (result == zkfperrdef.ZKFP_ERR_OK)
+            {
+                await ConvertToBitmapAndSendToApi(imgBuffer, fpInstance.imageWidth, fpInstance.imageHeight);
+            }
+            else
+            {
+                StatusLabel.ForeColor = errorColor;
+
+                if (dictionary.TryGetValue(result, out string? errorMessage))
+                {
+                    StatusLabel.Text = errorMessage;
+                }
+                else
+                {
+                    StatusLabel.Text = "No fue posible leer la huella";
+                }
+
+            }
+
+            ReadFingeprint.Enabled = true;
+        }
+
+        private static readonly HttpClient client = new() { Timeout = TimeSpan.FromSeconds(10) };
+
+        private async Task ConvertToBitmapAndSendToApi(byte[] imgBuffer, int width, int height)
+        {
+            Bitmap bmp = new(width, height, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+
+            System.Drawing.Imaging.ColorPalette palette = bmp.Palette;
+            for (int i = 0; i < 256; i++)
+                palette.Entries[i] = Color.FromArgb(i, i, i);
+            bmp.Palette = palette;
+
+            System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(
+                new Rectangle(0, 0, width, height),
+                System.Drawing.Imaging.ImageLockMode.WriteOnly,
+                System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+
+            Marshal.Copy(imgBuffer, 0, bmpData.Scan0, imgBuffer.Length);
+            bmp.UnlockBits(bmpData);
+
+            try
+            {
+                string tempFilePath = Path.Combine(Path.GetTempPath(), "huella.bmp");
+                bmp.Save(tempFilePath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                if (!File.Exists(tempFilePath))
+                {
+                    throw new FileNotFoundException("El archivo temporal no se creó correctamente");
+                }
+
+                var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:8080/fingerprint");
+                var content = new MultipartFormDataContent();
+
+                using var fileStream = File.OpenRead(tempFilePath);
+                var fileContent = new StreamContent(fileStream);
+                content.Add(fileContent, "file", "huella.bmp");
+                request.Content = content;
+
+                try
+                {
+                    var response = await client.SendAsync(request);
+                    response.EnsureSuccessStatusCode();
+
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var fingerprintResponse = JsonSerializer.Deserialize<FingerprintResponse>(responseContent);
+
+                    this.Invoke(new Action(() =>
+                    {
+                        if (fingerprintResponse != null)
+                        {
+                            UUIDLabel.Text = fingerprintResponse.uuid;
+                            UUID = fingerprintResponse.uuid;
+
+                            StatusLabel.ForeColor = successColor;
+                            StatusLabel.Text = "Huella leída exitosamente";
+
+                            ShowFingerprint.SizeMode = PictureBoxSizeMode.Zoom;
+                            ShowFingerprint.Image = bmp;
+                        }
+                        else
+                        {
+                            StatusLabel.ForeColor = errorColor;
+                            StatusLabel.Text = "Respuesta del servidor inválida";
+                        }
+                    }));
+                }
+                catch (HttpRequestException)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        StatusLabel.ForeColor = errorColor;
+                        StatusLabel.Text = "No fue posible enviar la huella al servidor";
+                    }));
+                }
+            }
+            catch (ExternalException)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    StatusLabel.ForeColor = errorColor;
+                    StatusLabel.Text = "No fue posible almacenar la huella";
+                }));
+            }
+        }
+
+        private void CopyUUID_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(UUID);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
+
+public class FingerprintResponse
+{
+    public required string uuid { get; set; }
+}
